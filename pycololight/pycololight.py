e8dbd9ec6086b62f80ed304a515950dff58f3053
BazaJayGee66 <jarrad.goodwin66@gmail.com>
2021-06-11 21:43:16 +1000

    Refactor constants to be seperated out

--- pycololight/pycololight.py
+++ pycololight/pycololight.py
@@ -1,5 +1,12 @@
 import socket
 
+from .constants import (
+    COMMAND_PREFIX,
+    CUSTOM_EFFECT_COLOURS,
+    CUSTOM_EFFECT_MODES,
+    DEFAULT_EFFECTS,
+)
+
 
 class ColourSchemeException(Exception):
     pass
@@ -22,151 +29,6 @@ class DefaultEffectExecption(Exception):
 
 
 class PyCololight:
-    COMMAND_PREFIX = "535a30300000000000"
-    CUSTOM_EFFECT_COLOURS = {
-        "Breath": {
-            "decimal": 128,
-            "colours": (
-                "Red, Green, Blue",
-                "Rainbow",
-                "Green",
-                "Azure",
-                "Blue",
-                "Purple",
-                "Red",
-                "Orange",
-                "Yellow",
-                "White",
-                "Green, Blue",
-            ),
-        },
-        "Shadow": {
-            "decimal": 138,
-            "colours": (
-                "Red, Yellow",
-                "Red, Green",
-                "Red, Purple",
-                "Red, Blue",
-                "Green, Yellow",
-                "Green, Azure",
-                "Green, Blue",
-                "Blue, Azure",
-                "Blue, Purple",
-                "Yellow, White",
-                "Red, White",
-                "Green, White",
-                "Azure, White",
-                "Blue, White",
-                "Purple, White",
-            ),
-        },
-        "Flash": {
-            "decimal": 153,
-            "colours": (
-                "Red, Green, Blue",
-                "Rainbow",
-                "Green",
-                "Azure",
-                "Blue",
-                "Purple",
-                "Red",
-                "Orange",
-                "Yellow",
-                "White",
-            ),
-        },
-        "Flicker": {
-            "decimal": 163,
-            "colours": (
-                "Red, Green, Blue",
-                "Rainbow",
-                "Green",
-                "Azure",
-                "Blue",
-                "Purple",
-                "Red",
-                "Orange",
-                "Yellow",
-                "White",
-            ),
-        },
-        "Scene": {
-            "decimal": 173,
-            "colours": (
-                "Birthday",
-                "Girlfriends",
-                "Friends",
-                "Workmates",
-                "Family",
-                "Lover",
-            ),
-        },
-        "Mood": {
-            "decimal": 179,
-            "colours": (
-                "Red",
-                "Orange",
-                "Yellow",
-                "Green",
-                "Grass",
-                "Azure",
-                "Blue",
-                "Pink",
-                "Gold",
-                "Color",
-                "True Color",
-            ),
-        },
-        "Selected": {
-            "decimal": 191,
-            "colours": ("Savasana", "", "Sunrise", "", "Unicorns"),
-        },
-    }
-    CUSTOM_EFFECT_MODES = [
-        ("01", "00"),
-        ("02", "00"),
-        ("05", "10"),
-        ("05", "30"),
-        ("05", "40"),
-        ("05", "50"),
-        ("05", "70"),
-        ("05", "80"),
-        ("05", "90"),
-        ("05", "a0"),
-        ("05", "b0"),
-        ("05", "c0"),
-        ("05", "00"),
-        ("05", "20"),
-        ("05", "30"),
-        ("06", "00"),
-        ("06", "10"),
-        ("06", "20"),
-        ("06", "30"),
-        ("06", "50"),
-        ("05", "f0"),
-        ("05", "10"),
-        ("05", "40"),
-        ("05", "50"),
-        ("06", "60"),
-        ("06", "70"),
-        ("06", "80"),
-    ]
-
-    DEFAULT_EFFECTS = {
-        "80s Club": "049a0000",
-        "Cherry Blossom": "04940800",
-        "Cocktail Parade": "05bd0690",
-        "Instagrammer": "03bc0190",
-        "Pensieve": "04c40600",
-        "Savasana": "04970400",
-        "Sunrise": "01c10a00",
-        "The Circus": "04810130",
-        "Unicorns": "049a0e00",
-        "Christmas": "068b0900",
-        "Rainbow Flow": "03810690",
-        "Music Mode": "07bd0990",
-    }
-
     def __init__(self, host, port=8900, default_effects=True):
         self.host = host
         self.port = port
@@ -175,7 +37,7 @@ class PyCololight:
         self._brightness = None
         self._colour = None
         self._effect = None
-        self._effects = self.DEFAULT_EFFECTS.copy() if default_effects else {}
+        self._effects = DEFAULT_EFFECTS.copy() if default_effects else {}
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def _switch_count(self):
@@ -214,8 +76,8 @@ class PyCololight:
         if colour not in self.custom_effect_colour_scheme_colours(colour_scheme):
             raise ColourException
 
-        starting_decimal = self.CUSTOM_EFFECT_COLOURS[colour_scheme]["decimal"]
-        colour_key = self.CUSTOM_EFFECT_COLOURS[colour_scheme]["colours"].index(colour)
+        starting_decimal = CUSTOM_EFFECT_COLOURS[colour_scheme]["decimal"]
+        colour_key = CUSTOM_EFFECT_COLOURS[colour_scheme]["colours"].index(colour)
         if mode in [13, 14, 15, 22, 23, 24]:
             # These modes have a lower starting decimal of 128
             starting_decimal = starting_decimal - 128
@@ -224,10 +86,10 @@ class PyCololight:
         return colour_hex
 
     def _mode_hex(self, mode):
-        if not 1 <= mode <= len(self.CUSTOM_EFFECT_MODES):
+        if not 1 <= mode <= len(CUSTOM_EFFECT_MODES):
             raise ModeExecption
 
-        return self.CUSTOM_EFFECT_MODES[mode - 1]
+        return CUSTOM_EFFECT_MODES[mode - 1]
 
     @property
     def count(self):
@@ -247,7 +109,7 @@ class PyCololight:
         else:
             self._on = False
             command = bytes.fromhex(
-                "{}{}{}".format(self.COMMAND_PREFIX, self._get_config("command"), "e1e")
+                "{}{}{}".format(COMMAND_PREFIX, self._get_config("command"), "e1e")
             )
             self._send(command)
 
@@ -260,7 +122,7 @@ class PyCololight:
         brightness_prefix = "f"
         command = bytes.fromhex(
             "{}{}{}{:02x}".format(
-                self.COMMAND_PREFIX,
+                COMMAND_PREFIX,
                 self._get_config("command"),
                 brightness_prefix,
                 int(brightness),
@@ -278,7 +140,7 @@ class PyCololight:
         colour_prefix = "00"
         command = bytes.fromhex(
             "{}{}{}{:02x}{:02x}{:02x}".format(
-                self.COMMAND_PREFIX, self._get_config("effect"), colour_prefix, *colour
+                COMMAND_PREFIX, self._get_config("effect"), colour_prefix, *colour
             )
         )
         self._colour = colour
@@ -292,7 +154,7 @@ class PyCololight:
     def effect(self, effect):
         command = bytes.fromhex(
             "{}{}{}".format(
-                self.COMMAND_PREFIX,
+                COMMAND_PREFIX,
                 self._get_config("effect"),
                 self._effects[effect],
             )
@@ -302,7 +164,7 @@ class PyCololight:
 
     @property
     def default_effects(self):
-        return list(self.DEFAULT_EFFECTS.keys())
+        return list(DEFAULT_EFFECTS.keys())
 
     @property
     def effects(self):
@@ -310,10 +172,10 @@ class PyCololight:
 
     def include_default_effects(self, effects):
         for effect in effects:
-            if effect not in self.DEFAULT_EFFECTS:
+            if effect not in DEFAULT_EFFECTS:
                 raise DefaultEffectExecption
 
-            self._effects[effect] = self.DEFAULT_EFFECTS[effect]
+            self._effects[effect] = DEFAULT_EFFECTS[effect]
 
     def add_custom_effect(self, name, colour_scheme, colour, cycle_speed, mode):
         cycle_speed_hex = self._cycle_speed_hex(int(cycle_speed), int(mode))
@@ -333,7 +195,7 @@ class PyCololight:
         self._effects[name] = custom_effect_hex
 
     def custom_effect_colour_schemes(self):
-        return list(self.CUSTOM_EFFECT_COLOURS.keys())
+        return list(CUSTOM_EFFECT_COLOURS.keys())
 
     def custom_effect_colour_scheme_colours(self, colour_scheme):
-        return list(filter(None, self.CUSTOM_EFFECT_COLOURS[colour_scheme]["colours"]))
+        return list(filter(None, CUSTOM_EFFECT_COLOURS[colour_scheme]["colours"]))
